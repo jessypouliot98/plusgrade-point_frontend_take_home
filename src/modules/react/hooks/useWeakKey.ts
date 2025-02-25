d7aebf15b3dbb04e9779be6f7254ab6cd2897b27
Jessy Pouliot <jessypouliot98@gmail.com>
2025-02-25 18:54:58 -0500

    wip

--- src/modules/react/hooks/useWeakKey.ts
+++ src/modules/react/hooks/useWeakKey.ts
@@ -4,7 +4,7 @@ import { useRef, useState } from "react";
  * Generates a unique React key based on object reference.
  * Useful for extracting out a unique value from a list item where no properties can be certain to be unique.
  */
-export function useWeakKey<T extends WeakKey>(prefix?: string) {
+export function useWeakKey<T extends WeakKey>(prefix = "") {
   const indexRef = useRef(0);
   const [weakMap] = useState(() => new WeakMap<T, string>());
 
@@ -12,11 +12,11 @@ export function useWeakKey<T extends WeakKey>(prefix?: string) {
     getWeakKey: (item: T) => {
       let key = weakMap.get(item);
       if (typeof key !== "string") {
-        key = indexRef.current.toString();
+        key = `${prefix}:${indexRef.current.toString()}`;
         indexRef.current++;
         weakMap.set(item, key);
       }
-      return (prefix ?? "") + key;
+      return key;
     }
   }
 }
