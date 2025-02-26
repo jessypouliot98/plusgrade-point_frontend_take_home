7bf3219d0989d666dc41a103c10757bbec49e056
Jessy Pouliot <jessypouliot98@gmail.com>
2025-02-25 19:21:46 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,39 @@
+import Link from "next/link";
+import { clsx } from "clsx";
+
+const SUPPORTED_YEARS = [
+  2019,
+  2020,
+  2021,
+  2022,
+]
+
+export const metadata = {
+  title: "Tax Calculator",
+}
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main className="max-w-screen-lg mx-auto bg-white">
-        <h1>Tax calculator</h1>
+      <main className="max-w-screen-lg min-h-screen mx-auto bg-white space-y-6 py-6 px-4">
+        <h1 className="text-4xl">Tax calculator</h1>
+        <nav>
+          <menu className="flex flex-wrap gap-2">
+            {SUPPORTED_YEARS.map((year) => (
+              <li key={year}>
+                <Link
+                  className={clsx(
+                    "transition-colors block px-2 py-1 rounded border border-gray-300",
+                    "bg-white hover:bg-blue-100"
+                  )}
+                  href={`/tax-calculator/${year}`}
+                >
+                  {`${year} calculator`}
+                </Link>
+              </li>
+            ))}
+          </menu>
+        </nav>
       </main>
     </div>
   );
